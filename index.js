a21748f3f36e4b09a62e07d08e4dfbab5588c799
David Palacios <davidpa7710@gmail.com>
2023-04-12 22:33:18 -0600

    feat: fix error

--- index.js
+++ index.js
@@ -14,9 +14,6 @@ function handleClick(event) {
         } else if (buttonId === "btn-4") {
             window.open("https://github.com/davidpa7710/", "_blank");
         }
-        else if (buttonId === "btn-7") {
-            window.open("https://api.whatsapp.com/send?phone=5540947590&text=Hello%20I'm%20[name]%20and%20I'm%20contacting%20you%20for%20[reason%20for%20contact].%20", "_blank");
-         }
         else if (buttonId === "btn-5") {
             window.open("mailto:davidpa7710@gmail.com?subject=Contact Request&body=Hello David, my name is [your name] and I would like to contact you for [reason for contact].%0D%0AGreetings, [your name]", "_blank");
         }
